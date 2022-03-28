e6c9c7bcd64e8ccf084d7ff51fd25ef7755eee30
PanLL00 <4553733@gmail.com>
2022-03-28 09:34:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 修改默认主机名
+sed -i 's/OpenWrt/MiWiFi-R3G/g' ./package/base-files/files/bin/config_generate
+
+# 修改默认管理IP
+sed -i 's/192.168.1.1/192.168.131.1/g' ./package/base-files/files/bin/config_generate
